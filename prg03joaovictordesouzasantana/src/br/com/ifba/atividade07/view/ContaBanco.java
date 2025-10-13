bac75200f50d1bbcdd0cdc224e51fecf4cd1e9a7
João Victor <>
2025-10-13 14:29:29 -0300

    ADD: Metodos getters e setters dos atributos

--- prg03joaovictordesouzasantana/src/br/com/ifba/atividade07/view/ContaBanco.java
+++ prg03joaovictordesouzasantana/src/br/com/ifba/atividade07/view/ContaBanco.java
@@ -16,6 +16,46 @@ public class ContaBanco {
     private String dono;
     private float saldo;
     private boolean status;
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     
     
 }
